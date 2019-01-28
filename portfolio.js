5c72013bf5d533ecb14381deb05a5d6984ef85de
Billy Angermeier <william.r.angermeier@gmail.com>
2019-01-27 20:18:46 -0500

    style changes

--- portfolio.js
+++ portfolio.js
@@ -11,3 +11,43 @@ $(".carousel-control-prev").click(function(){
 $(".carousel-control-next").click(function(){
   $("#discGolf").carousel("next")
 })
+
+$(".carousel-control-prev").click(function(){
+  $("#discGolf-mobile").carousel("prev")
+})
+
+$(".carousel-control-next").click(function(){
+  $("#discGolf-mobile").carousel("next")
+})
+
+$(".carousel-control-prev").click(function(){
+  $("#cookAtHome").carousel("prev")
+})
+
+$(".carousel-control-next").click(function(){
+  $("#cookAtHome").carousel("next")
+})
+
+$(".carousel-control-prev").click(function(){
+  $("#cookAtHome-mobile").carousel("prev")
+})
+
+$(".carousel-control-next").click(function(){
+  $("#cookAtHome-mobile").carousel("next")
+})
+
+$(".carousel-control-prev").click(function(){
+  $("#earbugs").carousel("prev")
+})
+
+$(".carousel-control-next").click(function(){
+  $("#earbugs").carousel("next")
+})
+
+$(".carousel-control-prev").click(function(){
+  $("#earbugs-mobile").carousel("prev")
+})
+
+$(".carousel-control-next").click(function(){
+  $("#earbugs-mobile").carousel("next")
+})
